f757b38c374cc5c2d8a55c0f4296ef2dd9f50561
Derek Smart <derek@grindaga.com>
2022-12-09 11:24:09 -0500

    look for multiple kernel versions in release
    Signed-off-by: Derek Smart <derek@grindaga.com>

--- cmd/kernels/kernels.go
+++ cmd/kernels/kernels.go
@@ -33,14 +33,41 @@ func main() {
 		return
 	}
 	for _, release := range releases {
-		if strings.Contains(release.GetTagName(), linuxVariant) {
-			for _, line := range strings.Split(release.GetBody(), "\r\n") {
-				if strings.Contains(line, "Linux kernel version:") {
-					kernels = append(kernels, strings.TrimPrefix(line, "- Linux kernel version: "))
+		if strings.Contains(linuxVariant, ",") {
+			lv := strings.Split(linuxVariant, ",")
+			for _, v := range lv {
+				if strings.Contains(release.GetTagName(), v) {
+					for _, line := range strings.Split(release.GetBody(), "\r\n") {
+						if strings.Contains(line, "Linux kernel version:") {
+							kernels = append(kernels, strings.TrimPrefix(line, "- Linux kernel version: "))
+						}
+					}
+				}
+			}
+		} else {
+			if strings.Contains(release.GetTagName(), linuxVariant) {
+				for _, line := range strings.Split(release.GetBody(), "\r\n") {
+					if strings.Contains(line, "Linux kernel version:") {
+						kernels = append(kernels, strings.TrimPrefix(line, "- Linux kernel version: "))
+					}
 				}
 			}
 		}
 	}
+	kernels = dedupe(kernels)
 	kernels, err := json.Marshal(kernels)
 	fmt.Println(string(kernels))
+}
+
+// https://go.dev/play/p/iyb97KcftMa
+func dedupe(strSlice []string) []string {
+	allKeys := make(map[string]bool)
+	list := []string{}
+	for _, item := range strSlice {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
 }
